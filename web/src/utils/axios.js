8109cc3e2895d944064460e442e0821147c8135c
John Lucas <joohnzay@gmail.com>
2024-06-13 02:04:15 -0300

    (update): endpoint

--- web/src/utils/axios.js
+++ web/src/utils/axios.js
@@ -1,17 +1,17 @@
-import axios from 'axios';
-
-const ipAddress = 'localhost';
-const port = 3333;
-
-export default axios.create({
-  baseURL: `http://${ipAddress}:${port}`,
-});
-
 // import axios from 'axios';
 
-// const ipAddress = 'https://api-prosel.onrender.com';
+// const ipAddress = 'localhost';
 // const port = 3333;
 
 // export default axios.create({
-//   baseURL: `${ipAddress}`,
+//   baseURL: `http://${ipAddress}:${port}`,
 // });
+
+import axios from 'axios';
+
+const ipAddress = 'https://api-prosel.onrender.com';
+// const port = 3333;
+
+export default axios.create({
+  baseURL: `${ipAddress}`,
+});
